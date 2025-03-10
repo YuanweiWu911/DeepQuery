572e735c122a79ca0b07864f6b8e0c74a36f4230
YuanweiWu911 <ywwu.pmo@gmail.com>
2025-03-10 15:43:13 +0800

    On branch main  Your branch is up to date with 'origin/main'. 	deleted:    __pycache__/app.cpython-310.pyc 	deleted:    __pycache__/config.cpython-310.pyc 	modified:   config.py 	deleted:    services/__pycache__/__init__.cpython-310.pyc 	deleted:    services/__pycache__/api_handler.cpython-310.pyc 	deleted:    services/__pycache__/audio_util.cpython-310.pyc 	deleted:    services/__pycache__/chat_handler.cpython-310.pyc 	deleted:    services/__pycache__/log_handler.cpython-310.pyc 	deleted:    services/__pycache__/voice_service.cpython-310.pyc 	deleted:    services/__pycache__/websocket_handler.cpython-310.pyc 	modified:   templates/index.html 	deleted:    test/__pycache__/__init__.cpython-310.pyc 	deleted:    test/__pycache__/test_chat_handler.cpython-310-pytest-8.3.5.pyc 	deleted:    test/__pycache__/test_process_response.cpython-310-pytest-8.3.5.pyc 	deleted:    utils/__pycache__/__init__.cpython-310.pyc 	deleted:    utils/__pycache__/tray_icon.cpython-310.pyc

--- config.py
+++ config.py
@@ -8,17 +8,18 @@ SSH_CONFIG = {
     'PASSWORD': os.getenv('SSH_PASSWORD', 'wjswyw119')
 }
 
-WEB_SEARCH_CONFIG = {'SERPER_API_KEY': os.getenv('SERPER_API_KEY', 'ebba2f3fd617ab2108b7b66cf41cf5b3a717815a')}
 
 WEBSOCKET_PORT = 8765
 HTTP_PORT = 8000
 
 # 模型配置
 MODELS = [
-    {"value": "qwq", "name": "qwq", "selected": True},
     {"value": "deepseek-r1:1.5b", "name": "deepseek-r1:1.5b", "selected": False},
     {"value": "deepseek-r1:7b", "name": "deepseek-r1:7b", "selected": False},
     {"value": "deepseek-r1:32b", "name": "deepseek-r1:32b", "selected": False},
     {"value": "deepseek-r1:70b", "name": "deepseek-r1:70b", "selected": False},
+    {"value": "qwq", "name": "qwq", "selected": True},
     {"value": "deepseek-r1:671b", "name": "deepseek-r1:671b", "selected": False}
 ]
+
+WEB_SEARCH_CONFIG = {'SERPER_API_KEY': os.getenv('SERPER_API_KEY', 'ebba2f3fd617ab2108b7b66cf41cf5b3a717815a')}
